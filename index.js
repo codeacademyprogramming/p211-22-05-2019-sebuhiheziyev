1d0e1351e07e0cc76454d14c1c61911583ba1399
sebuhiheziyev <49520955+sebuhiheziyev@users.noreply.github.com>
2019-05-23 11:56:35 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// let numbers = [15, 25, 50, 40, 17, 20];
+// let sam = 0; 
+// for(let i = 0; i < numbers.length-1;  i++ )
+// {
+//     sam += numbers[i];
+// }
+// console.log(sam)
+ 
+// function Array(n) {
+
+//     let sum=0;
+//     for(let i = 0; i <= n.length - 1;  i++)
+// {
+// sum+=n[i]
+// }
+
+
+// return sum;
+
+// }
+// let sum= Array([15, 20, 20, 85, 15]);
+// console.log(sum)
+  
+
+
+
+
+// 1
+// var adlar = prompt("ad?");
+// console.log( [adlar] );
+
+// 2
+// var max = Math.max(5, 10, 40, -20, 32);
+// console.log(max)
+
+
+3
+// var min = Math.min(5, 10, 40, -20, 32);
+// console.log(min)
+
+// 4
+// var max = Math.max(5, 10, 40, -20, 32);
+// var min = Math.min(5, 10, 50, 20, 32);
+// var sum= min + max
+// console.log(sum)
